aec688b060a6bb59f2b72db37b3d9551860fecff
Emma Drueke <emma.drueke@gmaila.com>
2018-01-05 12:00:41 -0600

    Cleaned up watchzoom.js and Added notes

--- watchzoom.js
+++ watchzoom.js
@@ -1,36 +1,49 @@
-//var chokidar = require('chokidar');
+
+var CronJob = require('cron').CronJob;
+new CronJob('* * * * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
+
+//Cron allows us run some functionality at specific time intervals 
+//The first paramater with all the '*' is where we format our range
+//In order it goes, Seconds, Minutes, Hours, Day of the Month,
+//Months, Day of the Week
+
+//The plan is to have a new CronJob running in the background looking
+//for updates to our Zoom file. 
+
+//--------------------------------------------------------------//
+//I tested out a couple different methods to watch a file. The File
+//System Library had a few methods that looked like they might be what
+//we needed, but fs.watch, fs.watchFile and fs.existsSync
+//fs.existsSync seems to work, but we did not get to test it much
+//I also found an extension of fs that someone created called Chokidar
+//It's incredibly confusing, and I didn't get it to work, but it seems
+//to have the most diverse functionality.
+//I have a code snippet below
+
+// var chokidar = require('chokidar');
+//One-liner for current directory, ignores .dotfiles
+// chokidar.watch('.', {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+//   console.log(event, path);
+// });
+
+//-------------------------------------------------------------//
+//Below is my attempt to put all the peices together. It doesn't
+//work just yet, but we are on the right path
 
 
+// const zoom = `/Users/%{process.env.USER}/Documents/Zoom`
 
 // var watcher = chokidar.watch(zoom, {
 //   ignored: /(^|[\/\\])\../,
 //   persistent: true
 // });
+
 // var log = console.log.bind(console);
-//-------------------------------------------------------------//
 
-// the above is another library that looked like it would be easier
-// to work with, but I can't seem to get it to work.
-
-// Also, this keeps saying that my file doesn't exist
-
-var fs = require('fs');
-const filePath = `/Users/%{process.env.USER}/Documents/Zoom`;
-const file = fs.readFileSync(filePath);
-console.log('Initial File content: ' + file);
-
-
-var CronJob = require('cron').CronJob;
-new CronJob('* * * * * *', function() {
-  fs.watch(filePath, function(event, fileName) {
-    if(fileName) {
-      console.log('Event: ' + event);
-      console.log(filename + ' File Change ...');
-      file = fs.readFileSync(filePath);
-      console.log("File content at: " + new Date() + ' is \n' + file);
-    }
-    else{
-      console.log('filename not provided')
-    }
-  });
-}, null, true, 'America/Los_Angeles');
+// var CronJob = require('cron').CronJob;
+// new CronJob('* * * * * *', function() {
+//   watcher.on('raw', (event, path, details) => {
+//     log('Raw event info:', event, path, details);
+// }, null, true, 'America/Los_Angeles')});
